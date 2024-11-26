eaa9d0d9a7631e339aa06b2d87db47d5e7d5c57e
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2024-11-26 23:01:42 +0200

    register, login, mood tracker
    mood tracker cration, register and login forms and components created, mood tracker with icons created. footer added

--- focusAndJoy/src/app/app.routes.ts
+++ focusAndJoy/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { ToDoComponent } from './to-do/to-do.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'to-do', component: ToDoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 
 ];
--- focusandjoy/src/app/home-page/home-page.component.ts
+++ focusandjoy/src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToDoComponent } from '../to-do/to-do.component';
+import { MoodComponent } from '../trackers/mood/mood.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [MoodComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
