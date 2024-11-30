999e602d8db7fc7ae5c862e8bf4fc1ac30ed20a8
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2024-11-30 16:32:43 +0200

    sleep component added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FlowerMoodComponent } from '../trackers/flower-mood/flower-mood.component';
+import { MoodComponent } from '../trackers/mood/mood.component';
+import { SleepComponent } from '../trackers/sleep/sleep.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FlowerMoodComponent],
+  imports: [FlowerMoodComponent,MoodComponent, SleepComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
